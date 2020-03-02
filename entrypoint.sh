c7b91225d9836f974c697284c85c4431d5f23a7d
Manuel Castellin <manuel@Manuels-Mac-mini.local>
2020-03-02 20:42:51 +0000

    chore: build SAP Commerce with customize task
    When building Hybris commerce, if no `ANT_TASKS` argument is provided,
    the entrypoint will run with `ant clean customize all` by default.
    
    By includind the `customze` task we make sure the build runs
    completely and includes all customizations.
    
    Fixes #3

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ then
   echo "Executing custom build tasks: $ANT_TASKS"
   ant $ANT_TASKS
 else
-  echo "Executing default build tasks: clean all"
-  ant clean all
+  echo "Executing default build tasks: clean customize all"
+  ant clean customize all
 fi
 
 ./hybrisserver.sh
